0c05a2fa5e6d7b9dcaad72bfba3db4854b17b334
energyshawn <shawn.bordeaux@gmail.com>
2015-09-15 23:53:18 -0500

    added new product

--- TestWebAPI/Controllers/ProductsController.cs
+++ TestWebAPI/Controllers/ProductsController.cs
@@ -14,7 +14,8 @@ namespace TestWebAPI.Controllers
         {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Foo", Category = "Barr", Price = 1.99M }
         };
 
         public IEnumerable<Product> GetAllProducts()
